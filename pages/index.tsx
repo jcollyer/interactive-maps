780ceeb531939fcd90c6622717394e5a6e6d51cd
jeremy collyer <collyerdesign@gmail.com>
2025-01-04 09:40:27 -0800

    add clipboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import prisma from "@/lib/prisma";
 import { Loader } from '@googlemaps/js-api-loader';
 import { Building } from '@prisma/client';
@@ -16,25 +16,27 @@ export const getStaticProps = async () => {
   };
 };
 
-
-export default function GoogleMaps({buildings}: {buildings: Building[]}) {
+export default function GoogleMaps({ buildings }: { buildings: Building[] }) {
   const mapRef = React.useRef<HTMLDivElement>(null);
+
+  const [clipboard, setClipboard] = useState<(string | null)[]>([]);
 
   function buildContent(property: Building) {
     const content = document.createElement("div");
     content.classList.add("property");
     content.innerHTML = `
-      <div class="icon">
-        <i aria-hidden="true" class="fa fa-icon fa-building" title="building"></i>
-        <span class="fa-sr-only">building</span>
-        <img src="${property.image}" alt="Property Image" />
-      </div>
+    <div class="flex flex-col p-2">
       <div class="details">
-        
-          <div class="font-bold">${property.address}</div>
-          <div>${property.altName}</div>
+        <div class="font-bold">${property.address}</div>
+        <div>${property.altName}</div>
       </div>
-      `;
+      <div class="relative bg-blue-600 p-1 rounded">
+        <i aria-hidden="true" class="fa fa-icon fa-building" title="building"></i>
+        <img src="${property.image}" alt="Property Image" />
+        <div class="arrow">&nbsp;</div>
+      </div>
+    </div>
+    `;
 
     return content;
   }
@@ -62,7 +64,7 @@ export default function GoogleMaps({buildings}: {buildings: Building[]}) {
       // Load the marker library
       await loader.importLibrary('marker') as google.maps.MarkerLibrary;
 
-      const locationInMap = { lat: 37.43238031167444, lng: -122.16795397128632 };
+      const locationInMap = { lat: 40.776676, lng: -73.971321 };
 
       const options: google.maps.MapOptions = {
         center: locationInMap,
@@ -76,12 +78,13 @@ export default function GoogleMaps({buildings}: {buildings: Building[]}) {
         const AdvancedMarkerElement = new google.maps.marker.AdvancedMarkerElement({
           map,
           content: buildContent(property),
-          position: {lat: property.lat || 0, lng: property.lng || 0},
+          position: { lat: property.lat || 0, lng: property.lng || 0 },
           title: property.address,
         });
 
         AdvancedMarkerElement.addListener("click", () => {
           toggleHighlight(AdvancedMarkerElement, property);
+          setClipboard((prev) => [...prev, property.address, ...(!!property.altName && [property.altName] || [])]);
         });
       }
     }
@@ -89,5 +92,23 @@ export default function GoogleMaps({buildings}: {buildings: Building[]}) {
     initializeMap();
   }, []);
 
-  return <div className="h-screen" ref={mapRef} />;
+  return (
+    <main>
+      <div className="h-screen" ref={mapRef} />
+      <div className="fixed bottom-0 p-4 bg-white w-full">
+        <div className="flex">
+        <div><h3 className="font-semibold">Copy Board</h3></div>
+        <div className="flex gap-2 ml-auto">
+          <button className="border px-4 py-1 rounded bg-blue-600 text-white" onClick={() => navigator.clipboard.writeText(clipboard.map(address => address).join(","))}>Copy</button>
+          <button className="border px-4 py-1 rounded"onClick={() => setClipboard([])}>Clear</button>
+        </div>
+        </div>
+        <div className="flex gap-2">
+          {clipboard.map((address, index) => (
+            <div key={index}>{address},</div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
